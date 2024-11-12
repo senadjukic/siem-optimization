9bc6490656a6ffd0a17699e677b5181a835e2521
Senad Jukic <10435409+senadjukic@users.noreply.github.com>
2024-11-12 16:21:25 +0100

    corrected openai commands

--- flink-sql/2.sql
+++ flink-sql/2.sql
@@ -1,6 +1,6 @@
 /*
 1. Provide login pattern 3x warning followed by a failure login event
-2. Create Users Table
+2. Create Users Table with sample data
 3. Enrich failed login attempts with user details
 4. Register OpenAI model
 5. Invoke OpenAI model with enriched context from the failed login attempts
@@ -32,7 +32,8 @@ CREATE TABLE FAILED_LOGIN_ATTEMPTS (
     `failure_time` STRING,
     `failure_source_ip` STRING,
     `failed_details` STRING,
-    PRIMARY KEY (username) NOT ENFORCED) AS
+    PRIMARY KEY (username) NOT ENFORCED) 
+    AS
         SELECT username,
               warning_count,
               failure_time,
@@ -99,31 +100,44 @@ INSERT INTO USER_DETAILS VALUES
     ('root', 'Anna', 'Kowalski', 'Polish', 'anna.kowalski@email.com'),
     ('guest', 'Marco', 'Rossi', 'Italian', 'marco.rossi@email.com'),
     ('user3', 'Lars', 'Anderson', 'Swedish', 'lars.anderson@email.com');
-  
 
 -- Enrich failed login attempts with user details
 CREATE TABLE ENRICHED_FAILED_LOGINS (
-  PRIMARY KEY (username) NOT ENFORCED
-  ) DISTRIBUTED BY HASH(username) INTO 1 BUCKETS 
-  AS SELECT u.username, CONCAT (
-    `language`, ', ', 
-    first_name, ', ', 
-    last_name, ', ', 
-    mail, ', ', 
-    CAST(warning_count AS STRING), ', ', 
-    f.failure_time, ', ', 
-    f.failure_source_ip, ', ', 
-    f.failed_details) AS big_string
-  FROM `FAILED_LOGIN_ATTEMPTS` f
-  INNER JOIN `USER_DETAILS` u
-  ON  f.username = u.username;
+  `username` STRING,
+  `big_string` STRING,
+  PRIMARY KEY (username) NOT ENFORCED) 
+  WITH (
+  'changelog.mode' = 'append'
+  );
+
+-- Generate Insert Statement, copy and execute the result
+SELECT DISTINCT -- Use DISTINCT if you want unique combinations
+    'INSERT INTO ENRICHED_FAILED_LOGINS (username, big_string) VALUES ' ||
+    '(''' || USER_DETAILS.username || ''', ''' ||
+    CONCAT(
+        `language`, ', ', 
+        first_name, ', ', 
+        last_name, ', ', 
+        mail, ', ', 
+        CAST(warning_count AS STRING), ', ', 
+        f.failure_time, ', ', 
+        f.failure_source_ip, ', ', 
+        f.failed_details
+    ) || ''');'
+FROM `FAILED_LOGIN_ATTEMPTS` f
+JOIN `USER_DETAILS`
+ON f.username = USER_DETAILS.username;
+
+-- Example copied from result above
+INSERT INTO ENRICHED_FAILED_LOGINS (username, big_string) VALUES ('system', 'French, Sophie, Dubois, sophie.dubois@email.com, 3, 2024-11-08T16:41:31.486178+00:00, 148.44.172.236, Security event details for log 000601150');
 
 /* Register OpenAI model - https://docs.confluent.io/cloud/current/flink/reference/functions/model-inference-functions.html
--- Create connection first via Flink Shell, make sure you have Confluent CLI > v4.7.0
+-- Create connection first via Confluent CLI, make sure you have Confluent CLI > v4.9.0
 confluent flink connection create openaiazure \
   --cloud azure \
   --region westeurope \
   --type azureopenai \
+  --environment <confluent-cloud-environment-id> \
   --endpoint https://xxx.openai.azure.com/openai/deployments/gpt-35-turbo/chat/completions?api-version=2024-08-01-preview \
   --api-key <api-key>
 */
@@ -134,7 +148,6 @@ OUTPUT (`user_mail` VARCHAR(2147483647))
 COMMENT 'create warning mail to user with failed login attemps'
 WITH (
   'azureopenai.connection' = 'openaiazure',
-  'azureopenai.model_version' = 'gpt-35-turbo',
   'azureopenai.system_prompt' = 'User has failed to login in our company system multiple times. Provided is security log in form of CSV message with values: language, first_name, last_name, mail, warning_count, failure_time, source_ip, log_id. Create response to the user in prefered language that summarises what happened. Write in JSON format: {"mail":"mail address", "mail":"generated mail message text"}',
   'provider' = 'azureopenai',
   'task' = 'text_generation'
@@ -142,3 +155,12 @@ WITH (
 
 -- Invoke OpenAI model with enriched context from the failed login attempts
 SELECT username, user_mail FROM ENRICHED_FAILED_LOGINS, LATERAL TABLE(ML_PREDICT('security_messages', big_string));
+
+-- Persist OpenAI answers into a table / topic
+CREATE TABLE OPENAI_GENERATED_EMAILS (
+    `username` STRING,
+    `email_message` STRING,
+    PRIMARY KEY (username) NOT ENFORCED
+);
+
+INSERT INTO `OPENAI_GENERATED_EMAILS` SELECT username, user_mail FROM ENRICHED_FAILED_LOGINS, LATERAL TABLE(ML_PREDICT('security_messages', big_string));
